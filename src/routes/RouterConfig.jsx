29040c269192c8412420e80d1f9536c5b63ac7f5
Sanghyuk-CH <nugu1223@gmail.com>
2024-03-02 14:55:44 +0900

    feat: add library
    react query
    recoil

--- src/routes/RouterConfig.jsx
+++ src/routes/RouterConfig.jsx
@@ -1,27 +1,33 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import {
-  ROUTES_PATH_ROOT,
-  ROUTES_PARAMS_ALL,
-  ROUTES_PATH_USERS
-} from '../constants/Routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
+import { ROUTES_PATH_ROOT, ROUTES_PARAMS_ALL, ROUTES_PATH_USERS } from '../constants/Routes';
 import BaseLayout from '../components/BaseLayout';
 import Home from '../pages/Home';
 import Users from '../pages/Users';
 
 export default function RouterConfig() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 5, // 5분
+        gcTime: 1000 * 60 * 10, // 10분
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
   return (
     <BaseLayout>
-      <Routes>
-        <Route path={ROUTES_PATH_ROOT} element={<Home />} />
-        <Route
-          path={`${ROUTES_PATH_USERS}/${ROUTES_PARAMS_ALL}`}
-          element={<Users />}
-        />
-        <Route
-          path={ROUTES_PARAMS_ALL}
-          element={<Navigate to={ROUTES_PATH_ROOT} />}
-        />
-      </Routes>
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route path={ROUTES_PATH_ROOT} element={<Home />} />
+            <Route path={`${ROUTES_PATH_USERS}/${ROUTES_PARAMS_ALL}`} element={<Users />} />
+            <Route path={ROUTES_PARAMS_ALL} element={<Navigate to={ROUTES_PATH_ROOT} />} />
+          </Routes>
+        </QueryClientProvider>
+      </RecoilRoot>
     </BaseLayout>
   );
 }
